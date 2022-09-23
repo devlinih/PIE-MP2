5dc456b8a0884301f43a4b178bcecdc5f3ef7c39
Devlin Ih <devlinih@protonmail.com>
2022-09-23 16:23:01 -0400

    Refactor to be more useful for writing a user interface.

--- visualizer/arduino.py
+++ visualizer/arduino.py
@@ -1,14 +1,14 @@
 """
 Connect to an Arduino running PIE MP2 Firmware.
+
+All Arduino related code is here.
 """
 
 import ast
 import serial
 import serial.tools.list_ports as list_ports
-import time
 
 BAUDRATE = 115200
-WAITTIME = 3
 
 # List of Arduino IDs provided by Brad
 # https://github.com/bminch/PIE/blob/main/Serial_cmd.py
@@ -33,27 +33,22 @@ def guess_port():
     return ""
 
 
-def scan(port=""):
+def scan(ser:serial.Serial):
     """
     Send a SCAN command to the Arduino.
 
     Raises an exception if no port is specified and no Arduino is found.
 
     Args:
-        port: A serial port to connect to. If not specified, try to find an
-            Arduino.
+        ser: A serial connection to an Arduiono.
 
     Returns:
         A list of tuples representing datapoints.
     """
-    if port == "":
-        port = guess_port()
+    ser.write(b"SCAN\n")
+    data = ser.readline()
+    # data is a bytestring, do some stuff to return it as a list
+    return ast.literal_eval(data.decode("utf-8").strip())
 
-    with serial.Serial(port, BAUDRATE, timeout=1) as ser:
-        # Sleep 1 second to let Arduino get ready
-        time.sleep(WAITTIME) # seconds to wait
-        ser.write(b"SCAN\n")
 
-        data = ser.readline()
-        # data is a bytestring, do some stuff to return it as a list
-        return ast.literal_eval(data.decode("utf-8").strip())
+# TODO: Add code to interface with calibration command
