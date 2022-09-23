041dc6b4c9c8a2a035ead419691304a6de5d439f
Devlin Ih <devlinih@protonmail.com>
2022-09-23 01:46:58 -0400

    Add very very verr pseudocode for a user interface

---
+++ visualizer/user_interface.py
@@ -0,0 +1,10 @@
+"""
+User interface for scanner
+"""
+
+# Unknown if it's gonna be a GUI, TUI, or CLI
+
+# Wrap main in a with statement for serial
+
+# Refactor Arduino serial functions to not create a new instance with with, but
+# rather be passed a serial instance. (more functional, much better).
